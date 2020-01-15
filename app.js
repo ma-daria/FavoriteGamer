4936e1e401410baa1624fff3d22e7ee5ce10c589
ma-daria <marina.dasha18@yandex.ru>
2020-01-16 00:31:23 +0300

    routes sign_up

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const signUpRouter = require('./routes/signUp');
 
-var app = express();
+const app = express();
 
 require('./database/lib/dbInit');
 require('dotenv').config();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sign_up', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
