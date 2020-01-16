49d5566064e6f478dea74670686e98624be51cb3
ma-daria <marina.dasha18@yandex.ru>
2020-01-16 16:12:25 +0300

    sign in

--- routes/signIn.js
+++ routes/signIn.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const inquiry_user = require('../database/inquiry/inquiry_user');
 
-router.post('/', async function(req, res) {
+router.get('/', async function(req, res) {
     let email = req.body.email;
     let password = req.body.password;
     res.end (await inquiry_user.SignIn(email, password));
